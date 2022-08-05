8d0865ad470807292eb4dc1b3fb1e6f35805a90c
WaterySaucer113pp <93452656+bbaj8@users.noreply.github.com>
2022-08-05 21:39:22 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -64,8 +64,8 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/QQQLO"
-)  # Example:- https://t.me/YY8GG
+    "SUPPORT_CHANNEL", "https://t.me/##$##22"
+)  # Example:- https://t.me/$#$2212
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP",
 )  # Example:- https://t.me/MUSICSOURCEDRAGON
